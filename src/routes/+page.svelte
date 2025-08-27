0df3325b6746f8529632dd006acffdf4f60bb558
mhuang03 <huangmichael150@gmail.com>
2025-08-27 00:42:13 -0500

    font choice + landing page poetry!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,24 @@
 <h1>Shlop</h1>
+
+<p>everything is slop</p>
+<p>nothing is slop</p>
+<p>shlop is slop</p>
+<p>you are slop</p>
+<p>i am slop</p>
+<p>we are all slop</p>
+<p>the universe is slop</p>
+<p>life is slop</p>
+<p>death is slop</p>
+<p>existence is slop</p>
+<p>non-existence is slop</p>
+<p>reality is slop</p>
+<p>illusion is slop</p>
+<p>time is slop</p>
+<p>space is slop</p>
+<p>matter is slop</p>
+<p>energy is slop</p>
+<p>consciousness is slop</p>
+<p>unconsciousness is slop</p>
+<p>everything is slop</p>
+<p>nothing is slop</p>
+<p>shlop is slop</p>
