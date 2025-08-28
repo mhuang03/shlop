456ec2f4f4a2decd9acc6ac26b18e063cbc4528c
mhuang03 <huangmichael150@gmail.com>
2025-08-28 01:28:26 -0500

    fix css scoping

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
   import "@fontsource/coral-pixels";
-  import "./404.css";
 </script>
+
+<svelte:head>
+  <style>
+    @import "./404.css";
+  </style>
+</svelte:head>
 
 <h1>404</h1>
 <p>shlop not found</p>
